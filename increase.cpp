c0f0357180ca0df3945b59419fb76fdce90dc9eb
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-09-19 10:28:58 -0400

    Create increase.cpp

---
+++ increase.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+  double num1;
+  double num2;
+  double num3;
+
+  std::cin >> num1;
+  std::cin >> num2;
+  std::cin >> num3;
+
+  if (num3 > num2 && num3 > num1){
+    std::cout << "Increasing \n";
+  }
+  else if (num1 > num2 && num2 > num3){
+    std::cout << "Decreasing \n";
+  }
+  else {
+    std::cout << "Neither \n";
+  }
+  }
